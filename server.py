6a26bf46be4845e24e7dc5b46b5ad3c17152c60a
Kramm <134699029+InvaderKrm@users.noreply.github.com>
2024-11-28 23:07:21 -0300

    Initial commit

---
+++ server.py
@@ -0,0 +1,40 @@
+from flask import Flask, request
+import base64
+import os
+import banco
+from datetime import datetime
+from seeImage import le_imagem
+from image_monitor import ImageMonitor
+
+ime = ImageMonitor()
+
+def get_timestamp():
+    """ Retorna a data e hora atual formatada como dd-mm-aaaa hh-mm-ss """
+    now = datetime.now()
+    return now.strftime("%d-%m-%Y %H-%M-%S")
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    # Chama o método estático da classe HomePage
+    return ime.home()
+
+
+
+@app.route('/upload', methods=['POST'])
+def upload_image():
+    data = request.data
+    with open("received_image.jpg", "wb") as file:
+        file.write(base64.b64decode(data))
+    filename = "imagem"+get_timestamp()+".jpg"
+    os.rename("received_image.jpg",filename)
+    txt = le_imagem(filename)
+    txt = banco.extrair_palavras_chave(txt)
+    txt = banco.encontrar_melhor_resultado(txt)
+    txt = txt['poema']
+    return txt, 200
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
